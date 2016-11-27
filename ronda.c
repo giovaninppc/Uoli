f05d4ebf05a0cd8974f3dd5f96293cc62ef45ad3
Giovani Nascimento Pereira <giovani.x.pereira@gmail.com>
2016-11-27 10:09:57 -0200

    Criado arquivo para ronda.c

---
+++ ronda.c
@@ -0,0 +1,13 @@
+/* Giovani Nascimento Pereira - 168609
+ * MC404 - Projeto 2 - Uoli / 2016/2S
+ * LoCo - ronda
+ * Coloca o Uoli na forma de ronda
+ * Ele se desloca em quadrados avancando a frente pouco a pouco
+ */
+
+#include "api_robot2.h"
+
+void _start(void){
+
+	
+}
